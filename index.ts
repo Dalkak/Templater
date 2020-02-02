a8be55b797901e1accfcb3c7b5c70d6acea10ef7
Gnlow <entrydark@gmail.com>
2020-02-02 16:51:11 +0900

    write main code

--- index.ts
+++ index.ts
@@ -5,14 +5,24 @@ import {
 export default new Pack({
     name: "templater",
     blocks: {
-        log: new Block({
-            name: "log",
-            template: "(text) 찍기",
-            func: (param, project, platform) => {
-                console.log(param.text);
-            },
-            params: {
-                text: Literal.from("안녕")
+        init: new Block({
+            name: "init",
+            template: "Templater 사용하기",
+            func: (param, project, platform: any) => {
+                var templater = valueField => (object, script) => {
+                    var value = script.getField(valueField, script);
+                    if(/{.*?}/g.exec(value)[0] == value){
+                        return project.variables.value[value.substring(1, value.length-1)].value
+                    }else{
+                        return value
+                            .replace(
+                                /{.*?}/g, 
+                                val => project.variables.value[val.substring(1, val.length-1)].value
+                            );
+                    }
+                }
+                platform.Entry.block.text.func = templater("NAME");
+                platform.Entry.block.number.func = templater("NUM");
             }
         })
     }
